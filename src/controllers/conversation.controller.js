d1310a9810820eef1ba37798d4caae48a2e2c5cb
Bryan Emmanuel <boboskymusa@gmail.com>
2026-06-26 20:39:27 +0100

    messages

--- src/controllers/conversation.controller.js
+++ src/controllers/conversation.controller.js
@@ -1,4 +1,7 @@
 const conversationService = require("../services/conversation.service")
+const asyncHandler =
+  require("../utils/asyncHandler");
+
 exports.startConversation =
 asyncHandler(
   async (req, res) => {
@@ -35,6 +38,65 @@ asyncHandler(
       success: true,
       data:
         message,
+    });
+  }
+);
+
+exports.getInbox = asyncHandler(
+  async (req, res) => {
+    const conversations =
+      await conversationService.getInbox(
+        req.user.id
+      );
+
+    res.status(200).json({
+      success: true,
+      data: conversations,
+    });
+  }
+);
+
+exports.getMessages = asyncHandler(
+  async (req, res) => {
+
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 20;
+
+    const messages =
+      await conversationService.getMessages(
+        req.params.id,
+        page,
+        limit
+      );
+
+    res.status(200).json({
+      success: true,
+      data: messages.rows,
+      pagination: {
+        total: messages.count,
+        page,
+        limit,
+        totalPages: Math.ceil(
+          messages.count / limit
+        ),
+      },
+    });
+  }
+);
+
+exports.unreadCount = asyncHandler(
+  async (req, res) => {
+
+    const unreadCount =
+      await conversationService.unreadCount(
+        req.user.id
+      );
+
+    res.status(200).json({
+      success: true,
+      data: {
+        unreadCount,
+      },
     });
   }
 );
